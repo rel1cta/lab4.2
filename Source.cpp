05c236b5acd9ef21b84e843e46a2c083e3fa8ec9
rel1cta <dimksha036@gmail.com>
2022-10-20 22:00:57 +0300

    lab4.2

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = tan(x)+1;
+		if (x <= -5)
+			B = exp(x / (1 - x));
+		else
+			if (-5 < x && x <= 3)
+				B = 3.2 + log10(1.8 * pow(x, 2));
+			else
+				B = abs(x) * sqrt(x);
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
